088435fae2314ae7753a13664210a2cf2e2a1b3e
Richard Melchior <raustin.melchior@gmail.com>
2018-06-15 19:06:15 +0000

    automatically backed up by learn

--- db/migrate/011_add_actor_id_and_show_id_to_characters.rb
+++ db/migrate/011_add_actor_id_and_show_id_to_characters.rb
@@ -1,6 +1,5 @@
 class AddActorIdAndShowIdToCharacters  < ActiveRecord::Migration[4.2]
   def change
     add_column(:characters, :actor_id, :integer)
-    add_column(:characters, :show_id, :integer)
   end
 end
