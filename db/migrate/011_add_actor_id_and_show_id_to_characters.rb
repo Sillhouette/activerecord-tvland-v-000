20ca21c9c8ec7bfd7a4c8cc6fd4e757417efbe6e
Richard Melchior <raustin.melchior@gmail.com>
2018-06-15 19:05:53 +0000

    automatically backed up by learn

--- db/migrate/011_add_actor_id_and_show_id_to_characters.rb
+++ db/migrate/011_add_actor_id_and_show_id_to_characters.rb
@@ -1,5 +1,6 @@
-class AddNetworkIdToShows  < ActiveRecord::Migration[4.2]
+class AddActorIdAndShowIdToCharacters  < ActiveRecord::Migration[4.2]
   def change
-    add_column(:shows, :network_id, :integer)
+    add_column(:characters, :actor_id, :integer)
+    add_column(:characters, :show_id, :integer)
   end
 end
