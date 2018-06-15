5b8ad8beae0e4bc35c06788074ee0334ba0969f3
Richard Melchior <raustin.melchior@gmail.com>
2018-06-15 18:59:05 +0000

    automatically backed up by learn

--- db/migrate/011_add_actor_id_and_show_id_to_characters.rb
+++ db/migrate/011_add_actor_id_and_show_id_to_characters.rb
@@ -1,4 +1,4 @@
-class AddForeignKeyColumns  < ActiveRecord::Migration[4.2]
+class AddActorIdAndShowIdToCharacters  < ActiveRecord::Migration[4.2]
   def change
     add_column(:characters, :actor_id, :integer)
     add_column(:characters, :show_id, :integer)
